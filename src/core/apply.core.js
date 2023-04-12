5d13e0e8a978ec9c2045c2e508d497f3a62241cc
azder <azhder@gmail.com>
2023-04-12 20:39:47 +0200

    minor apply refactor

--- src/core/apply.core.js
+++ src/core/apply.core.js
@@ -1,22 +1,21 @@
 import ET from '#src/etc/et.const.js';
-import {V} from '#src/etc/field.const.js';
+import {ES_TYPE, V} from '#src/etc/field.const.js';
 import X from '#src/index.js';
-import estype from '#src/util/estype.util.js';
 
 
 const apply = (target, that, args) => {
 
-    // eslint-disable-next-line no-use-before-define
     if (!X.instance(target)) {
-        return Reflect.apply(target, that, args);
+        // eslint-disable-next-line new-cap
+        return X(Reflect.apply(target, that, args));
     }
 
-    if (ET.fun !== estype(target[V])) {
-        return target;
+    if (ET.fun === (target[ES_TYPE])) {
+        // eslint-disable-next-line no-use-before-define,new-cap
+        return X(target[V].apply(that, args));
     }
 
-    // eslint-disable-next-line no-use-before-define,new-cap
-    return X(target[V].apply(that, args));
+    return target;
 };
 
 
